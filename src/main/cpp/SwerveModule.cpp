a0ee20b64a7a920afd07c92eda6d5b78fdbedeed
jrdunston94 <136032335+jrdunston94@users.noreply.github.com>
2025-09-18 10:23:13 -0400

    Added Conversion factors do Drive

--- src/main/cpp/SwerveModule.cpp
+++ src/main/cpp/SwerveModule.cpp
@@ -1,4 +1,5 @@
 #include "SwerveModule.h"
+#include <units/math.h>
 
 SwerveModule::SwerveModule(int Driveid,int Turnid)
 :
@@ -10,14 +11,21 @@ rev::spark::SparkBaseConfig configdrive{};
   configdrive.SmartCurrentLimit(50);
   configdrive.SetIdleMode(rev::spark::SparkBaseConfig::kCoast);
 //TODO:Get position conversion factor by doing math- gear ratio to the wheel and wheel diameter would reccomend this to be in meters there is a units library in C++
-//TODO: Velocity conversion factor for the drive - Position unit conversion factor dived by 60 to get
+//2025 Comp drivebase is 6.23:1
+//2024 Comp drivebase is 3.75:1
+  
+  //TODO: Velocity conversion factor for the drive - Position unit conversion factor dived by 60 to get
 
-int WheelDiameter= 4;
+Double WheelDiameter = 3; //Value in inches
+Double GearRatio = 3.75; 
+Double PConversion= WheelDiameter*PI()/GearRatio;
+Double VConversion= PConversion/60;
+
   //Provide these values above to here
   //Look in the REV library for additional desired functions, disable soft limits on all controllers
   //
-  configdrive.encoder.PositionConversionFactor();
-  configdrive.encoder.VelocityConversionFactor();
+  configdrive.encoder.PositionConversionFactor(PConversion);
+  configdrive.encoder.VelocityConversionFactor(VConversion);
   configdrive.closedLoop.Pidf(0.06,0,0,0);
 
   //switch this to the internal encoder
@@ -44,4 +52,5 @@ int WheelDiameter= 4;
                           rev::spark::SparkBase::ResetMode::kNoResetSafeParameters,
                           rev::spark::SparkBase::PersistMode::kNoPersistParameters);
 }
+
 }
